86c24243035ad56e424152e4311be4a2f9bcfd58
DAVV422 <diegoalberto42216@gmail.com>
2025-05-09 22:18:55 -0400

    feat(organizacion): configure OrganizacionModule with TypeOrm and service/controller dependencies

--- src/modules/organizacion/organization.module.ts
+++ src/modules/organizacion/organization.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrganizacionEntity } from './entities/organizacion.entity';
+import { CuentaModule } from '../cuenta/cuenta.module';
+import { OrganizacionService } from './services/organizacion.service';
+import { OrganizacionController } from './controllers/organizacion.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([]),
+    TypeOrmModule.forFeature([OrganizacionEntity]),
+    CuentaModule
   ],
-  controllers: [],
-  providers: [],
-  exports:[ TypeOrmModule,  ]
+  controllers: [OrganizacionController],
+  providers: [OrganizacionService],
+  exports:[ TypeOrmModule, OrganizacionService ]
 })
 export class OrganizacionModule {}
