39a505dca23edc2083a54771bc1fe73871b528c8
DAVV422 <diegoalberto42216@gmail.com>
2025-05-13 23:12:11 -0400

    feat(auth): enhance PermisoGuard to validate user permissions using rolService and authToken

--- src/auth/guards/permisos.guard.ts
+++ src/auth/guards/permisos.guard.ts
@@ -5,12 +5,16 @@ import { Request } from 'express';
 import { PERMISOS } from '../constants/permisos.constant';
 import { PERMISOS_KEY } from '../decorators/permiso.decorator';
 import { PermisoRolService } from '../services/permission-role.service';
+import { IAuthToken } from '../interfaces/authToken.interface';
+import { authToken } from '../../common/utils/auth.token';
+import { RolService } from '../services/rol.service';
 
 @Injectable()
 export class PermisoGuard implements CanActivate {
 
     constructor(
         private readonly reflector: Reflector,
+        private readonly rolService: RolService,
         private readonly permisoRolService: PermisoRolService,
     ) { }
 
@@ -18,13 +22,17 @@ export class PermisoGuard implements CanActivate {
         context: ExecutionContext,
     ): Promise<boolean> {
         try {
-
             const permissionRequire = this.reflector.get<Array<PERMISOS>>(PERMISOS_KEY, context.getHandler());
             const request = context.switchToHttp().getRequest<Request>();
+            const token = request.headers.authorization?.split(' ')[1];
+            if (!token || Array.isArray(token))
+                    throw new UnauthorizedException('Token no encontrado');
+            const managerToken: IAuthToken | string = authToken(token);
+            // const rol = managerToken.
+            const rol =  await this.rolService.findOneByName((managerToken as IAuthToken).rol!);
+            // console.log(rol);
             if (permissionRequire === undefined || permissionRequire.length == 0) return true;
-
-            const { rolId } = (request as any);
-            const permissionsUser = await this.permisoRolService.getPermisosOfRol(rolId);
+            const permissionsUser = await this.permisoRolService.getPermisosOfRol(rol.id);
             const permissionArrayName = permissionsUser.map((permission) => permission.permiso.nombre);
 
             let isAuthorized = false;
