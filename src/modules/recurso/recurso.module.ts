76ff365e893a48e0759a44b48d70b075f6a71924
DAVV422 <diegoalberto42216@gmail.com>
2025-07-06 14:01:41 -0400

    refactor(recurso): update import paths for consistency in RecursoModule

--- src/modules/recurso/recurso.module.ts
+++ src/modules/recurso/recurso.module.ts
@@ -2,10 +2,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecursoEntity } from './entities/recurso.entity';
-import { MaquinaEntity } from '../maquina/entities/maquina.entity';
+import { MaquinaEntity } from './../maquina/entities/maquina.entity';
 import { RecursoService } from './services/recurso.service';
 import { RecursoController } from './controllers/recurso.controller';
-import { CuentaModule } from '../cuenta/cuenta.module'; // Asumiendo que CuentaModule es una dependencia común
+import { CuentaModule } from './../cuenta/cuenta.module'; 
 
 @Module({
     imports: [
